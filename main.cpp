9db39721413e2f3e76e2c4ad21c23948e19ccd3c
rubicks <rubicks00@gmail.com>
2014-07-27 19:00:02 -0400

    got boost checking working

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 /* calc/main.cpp */
 
+
 #include<iostream>
 
-int main( int argc, char**argv )
+
+#ifdef HAVE_CONFIG_H
+#  include"config.h"
+#else
+#  error"config.h"
+#endif
+
+#ifdef HAVE_BOOST_THREAD_HPP
+#  include<boost/thread.hpp>
+#else
+#  error"boost/thread.hpp"
+#endif
+
+
+namespace
+{
+
+    struct callable
+    {
+        void
+        operator()( void )
+        {
+            std::cout << __PRETTY_FUNCTION__ << std::endl ;
+        }
+    };
+}
+
+int
+main( int argc, char**argv )
 {
     std::cout << __PRETTY_FUNCTION__ << std::endl ;
+
+    callable c;
+
+    boost::thread t( c );
+    t.join();
+    
     return 0 ;
 }
